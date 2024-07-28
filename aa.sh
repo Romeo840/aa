d6dbd0c2470db7e617ac3d4edd67092939b77c46
Romeo840 <88234123+Romeo840@users.noreply.github.com>
2024-07-28 23:24:45 +0530

    Create aa.sh

---
+++ aa.sh
@@ -0,0 +1,9 @@
+Codes: https://github.com/Romeo840/aa/new/blob/main/aa.sh
+
+git clone https://github.com/Romeo840
+
+cd aa.sh
+
+chmod +x aa.sh && ./aa.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
